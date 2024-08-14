5ed1464c5fb01b253c335e60f07bacfb5a8b2b3c
Jaheda20 <jahedasultana20@gmail.com>
2024-08-14 23:35:10 +0200

    banner section is added on homepage

--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -1,18 +1,18 @@
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaArrowRightFromBracket } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
-
+import logo from '/images/logo2.png'
 const Nav = () => {
 
     const navOptions = (
         <>
-            <li><NavLink className={({ isActive }) => isActive ? 'text-blue-800 font-bold md:text-lg' : "md:text-lg font-semibold"} to="/">Home</NavLink></li>
-            <li><NavLink className={({ isActive }) => isActive ? 'text-blue-800 font-bold md:text-lg' : "md:text-lg font-semibold"} to="/about">About</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-yellow-200 font-bold md:text-lg' : "text-blue-100 md:text-lg font-semibold"} to="/">Home</NavLink></li>
+            <li><NavLink className={({ isActive }) => isActive ? 'text-yellow-200 font-bold md:text-lg' : "text-blue-100 md:text-lg font-semibold"} to="/about">About</NavLink></li>
         </>
     )
 
     return (
-        <div className="navbar absolute bg-transparent mx-auto w-full">
+        <div className="navbar absolute bg-transparent mx-auto w-full z-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -34,22 +34,27 @@ const Nav = () => {
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
                         {navOptions}
                         <li>
-                            <Link className="flex items-center"><FaArrowRightFromBracket />
+                            <Link className="flex items-center "><FaArrowRightFromBracket />
                             Login</Link>
                         </li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-3xl md:text-5xl gap-0 text-blue-800 font-extrabold">
+                <div className="flex items-center">
+                {/* <img src={logo} alt="" className="w-32 text-yellow-200"/> */}
+                <a className="btn btn-ghost text-3xl md:text-5xl gap-0 text-blue-500 font-extrabold">
                     
-                    Auto
-                    <span className="text-slate-500 ">Lynx</span></a>
+                    A
+                    <span className="text-yellow-500 text-3xl md:text-5xl font-semibold ">utoLynx</span></a>
+
+                </div>
+                
             </div>
             
             <div className="navbar-end hidden lg:flex items-center">
                 <ul className="menu menu-horizontal px-1">
                     {navOptions}
                 </ul>
-                <Link className="flex items-center gap-2 ml-6 btn btn-ghost text-lg font-semibold">
+                <Link className="flex items-center gap-2 ml-6 btn btn-ghost text-lg font-semibold text-blue-100">
                 <FaRegUserCircle />
                 Login
                 </Link>
